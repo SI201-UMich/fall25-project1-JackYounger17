335b17879bb829a8228152cc1d3cff783db264d8
youngej3 <jack.younger3@mail.dcu.ie>
2025-10-08 19:29:27 +0100

    avg_yield function

--- jack_younger_project1.py
+++ jack_younger_project1.py
@@ -14,3 +14,22 @@ def load_data(filename):
             data.append(line)
 
     return data
+
+def avg_yield_per_crop(data):
+
+    crop_yield = {}
+    for row in data:
+        crop = row["Crop"]
+        yield_per_hectare = float(row["Yield_tons_per_hectare"])
+
+        if crop not in crop_yield:
+            crop_yield[crop] = []
+        crop_yield[crop].append(yield_per_hectare)
+
+    avg_yield = {}
+    for crop, values in crop_yield.items():
+        total = sum(values)
+        count = len(values)
+        avg_yield[crop] = total / count
+
+    return avg_yield
